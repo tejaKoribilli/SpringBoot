f9de1a903cb101df07760305df69efc77183c762
tejaKoribilli <tejakoribilli8892@gmail.com>
2023-09-05 07:22:43 +0000

    recipe test cases failed

---
+++ src/main/java/com/example/recipe/RecipeRepository.java
@@ -0,0 +1,18 @@
+// Write your code here
+package com.example.recipe;
+
+import java.util.*;
+
+public interface RecipeRepository{
+
+    ArrayList<Recipe> getRecipes();
+
+    Recipe getRecipeById(int recipeId);
+
+    Recipe addRecipe(Recipe recipe);
+
+    Recipe updateRecipe(int recipeId, Recipe recipe);
+
+    void deleteRecipe(int recipeId);
+    
+}
